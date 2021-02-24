770c714fb0fc2e0a64f56724a065251ca968c9df
Oscar TENG <betterteng@gmail.com>
2021-02-25 02:58:20 +1100

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/betterteng/Haha/HahaApplication.java
@@ -0,0 +1,14 @@
+package com.betterteng.Haha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HahaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HahaApplication.class, args);
+	}
+
+}
